95219b1905c6ca447a69351d85297a3f36955ffa
Danpr1984 <daniel.porras.rosses@gmail.com>
2022-02-16 17:44:21 +0000

    Add stock function to file

--- run.py
+++ run.py
@@ -13,7 +13,6 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-
 def get_sales_data():
     """
     Get sales figures input from the user
@@ -34,7 +33,6 @@ def get_sales_data():
             print("Data is valid")
             break
 
-
     return sales_data
 
 def validate_data(values):
@@ -54,7 +52,6 @@ def validate_data(values):
         return False
 
     return True
-
 
 def update_worksheet(data, worksheet):
     """
@@ -86,7 +83,6 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data    
 
-
 def get_last_5_entries_sales():
     """
     Collects collumns of data from sales worksheet, collecting
@@ -99,7 +95,24 @@ def get_last_5_entries_sales():
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
-    pprint(columns) 
+
+    return columns
+
+def calculate_stock_data(data):   
+    """
+    Calculate the average stock data
+    """
+    print("Calculating stock data") 
+    new_stock_data = []
+    
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column) 
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+
 
 def main():
     """
@@ -111,21 +124,23 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
-    
-    
-    
-   
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
+
+
 
 print("Welcome to Love Sandwiches Data Automation")
+
 main()
 
-sales_columns = get_last_5_entries_sales()
 
 
 
 """ REFACTORING------
 def update_sales_worksheet(data):
 
+   
 
     Update sales worksheet, add new row with the list data provided
 
@@ -145,3 +160,4 @@ def update_surplus_worksheet(data):
     print("Surplus worksheet updated successfully.\n")
 
 ------REFACTORING """     
+
