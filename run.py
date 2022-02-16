b016fc7e462b584e987a4108c510fe2fb6c30f49
Danpr1984 <daniel.porras.rosses@gmail.com>
2022-02-16 13:35:29 +0000

    Add Refactoring function for sales and surplus worksheets

--- run.py
+++ run.py
@@ -54,12 +54,12 @@ def validate_data(values):
 
     return True
 
-
+""" REFACTORING------
 def update_sales_worksheet(data):
 
-    """
+
     Update sales worksheet, add new row with the list data provided
-    """   
+
     print("Updating sales worksheet...\n")
     sales_worksheet = SHEET.worksheet('sales')
     sales_worksheet.append_row(data)
@@ -67,28 +67,28 @@ def update_sales_worksheet(data):
 
 
 def update_surplus_worksheet(data):
-    """
+    
     Update sales worksheet, add new row with the list data provided
-    """   
+    
     print("Updating sales worksheet...\n")
     surplus_worksheet = SHEET.worksheet('surplus')
     surplus_worksheet.append_row(data)
     print("Surplus worksheet updated successfully.\n")
 
-   
+------REFACTORING """     
 
-""" REFACTORING------
+
 def update_worksheet(data, worksheet):
-    
+    """
     Receives a list of integers to be inserted into a worksheet
     Update the relevant worksheet with the data provided
-    
+    """
 
     print(f'Updating {worksheet} worksheet...\n')
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f'{worksheet} worksheet updated successfully') 
- ------REFACTORING """    
+   
 
 
 def calculate_surplus_data(sales_row):
@@ -117,9 +117,10 @@ def main():
 
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
+    
     
     
    
@@ -128,7 +129,8 @@ print("Welcome to Love Sandwiches Data Automation")
 main()
 
 """
-update_worksheet(sales_data, "sales")
 
-update_worksheet(new_surplus_data, "surplus")
+update_sales_worksheet(sales_data)
+
+update_surplus_worksheet(new_surplus_data)
 """
